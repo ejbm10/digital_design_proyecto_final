6e3d617e5e234dc58fe0d752cb271cf71cab8455
ejbm10 <eduardo.bm10@estudiantec.cr>
2025-06-04 22:16:43 -0600

    Cambio de inputs a la memoria

--- hardware/RAM.sv
+++ hardware/RAM.sv
@@ -2,34 +2,30 @@ module RAM (
 	input logic clk,
 	input logic rst,
 	input logic MemWrite,
-	input logic [31:0] address,
 	input logic [31:0] A,
+	input logic [31:0] WriteData,
 	output logic [31:0] ReadData
 );
 
-	logic [6:0] addr_index;
-	logic [31:0] board [99:0];
+	logic [31:0] MemorySpace [0:99];
 	
 	always_comb begin
-		if (address >= 32'h1000 && address < 32'h1190)
-			addr_index = (address - 32'h1000) >> 2;
+		if (A >= 32'h1000 && A < 32'h1190)
+			ReadData = MemorySpace[(A - 32'h1000) >> 2];
 		else
-			addr_index = 0;
-	end
-	
-	always_comb begin
-		assign ReadData = board[addr_index];
+			ReadData = 32'hFFFF;
 	end
 	
 	always_ff @(negedge clk or posedge rst) begin
 		if (rst) begin
 			for (int i = 0; i < 100; i++) begin
-				board[i] <= 32'h0;
+				MemorySpace[i] <= 32'h0;
 			end
 		end
-		else if (MemWrite && address >= 32'h1000 && address < 32'h1190) begin
-			board[addr_index] <= A;
-		end
+		else if (MemWrite)
+			MemorySpace[(A - 32'h1000) >> 2] <= WriteData;
 	end
+	
+	
 	
 endmodule
--- testbenches/RAM_tb.sv
+++ testbenches/RAM_tb.sv
@@ -1,54 +1,54 @@
 module RAM_tb();
 	
-	logic clk, rst, r, w;
-	logic [31:0] address, mem_in, mem_out;
+	logic clk, rst, MemWrite;
+	logic [31:0] A, WriteData, ReadData;
 	
 	RAM dut (
 		.clk(clk),
 		.rst(rst),
-		.r(r),
-		.w(w),
-		.address(address),
-		.mem_in(mem_in),
-		.mem_out(mem_out)
+		.MemWrite(MemWrite),
+		.A(A),
+		.WriteData(WriteData),
+		.ReadData(ReadData)
 	);
 	
 	always #5 clk = ~clk;
 	
 	initial begin
 		clk = 0;
+		MemWrite = 0;
+		A = 32'h1000;
+		WriteData = 32'h0;
+		rst = 1;
+		#10;
 		rst = 0;
-		r = 0;
-		w = 0;
 		
-		address = 32'h1000;
-		mem_in = 32'h0;
+		#500;
 		
-		#100;
+		MemWrite = 1;
+		WriteData = 32'h13FF;
 		
-		w = 1;
-		mem_in = 32'h13FF;
+		#10; 
+		MemWrite = 0;
+		#10;
 		
-		#100;
 		
-		address = 32'h1004;
-		mem_in = 32'h100;
+		A = 32'h1004;
+		MemWrite = 1;
+		WriteData = 32'h100;
 		
-		#100;
+		#10;
+		MemWrite = 0;
+		#10;
 		
-		address = 32'h1000;
-		r = 1;
-		w = 0;
 		
-		#100;
+		A = 32'h1000;
 		
-		address = 32'h1004;
+		#500;
 		
-		#100;
+		A = 32'h1004;
 		
-		r = 0;
-		
-		#100;
+		#500;
 	end
 
 endmodule
