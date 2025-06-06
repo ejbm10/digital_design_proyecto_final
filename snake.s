5101fc523a191efdb56d2ae567be4bd5e097d054
KendallMM <86016849+KendallMM@users.noreply.github.com>
2025-06-06 11:34:27 -0600

    [Update

--- snake.s
+++ snake.s
@@ -4,8 +4,7 @@
 @ R8 = nivel actual 
 
 _start:
-    LDR R9, =0x1000            @ Dirección base del teclado simulado 
-    LDR R10, =0x2000           @ Dirección base de la matriz
+    LDR R9, =0x1000            @ Dirección base del teclado simulado @ Dirección base de la matriz: 0x2000
     MOV R8, #1                 @ Nivel inicial = 1
     BL init_game_level1        @ Inicializa el tablero y obstáculos del nivel 1
     MOV R7, #2                 @ Tamaño inicial de la serpiente (cabeza + cuerpo)
@@ -27,7 +26,7 @@ main_game_loop:
     B main_game_loop           @ sigue en el bucle principal
 
 check_level1:
-    CMP R7, #11                @ validacion de tamaño 11
+    CMP R7, #3                @ validacion de tamaño 11
     BLT main_game_loop         
     MOV R8, #2                 @ Pasa a nivel 2
     BL init_game_level2        @ Inicializa el tablero para nivel 2
@@ -59,35 +58,42 @@ fill_loop1:
     STR R2, [R0], #4
     SUBS R12, R12, #1
     BNE fill_loop1
+
+
+@ ===== Nivel1 ===== Obstáculos
     LDR R0, =0x2000
     MOV R3, #4
 
-    @ Valor 4: obstaculos
-    STR R3, [R0, #(2*10 + 1)*4]
-    STR R3, [R0, #(2*10 + 2)*4]
-    STR R3, [R0, #(2*10 + 3)*4]
-    STR R3, [R0, #(2*10 + 6)*4]
-    STR R3, [R0, #(2*10 + 7)*4]
+    STR R3, [R0, #( 3*10 + 1 )*4]
+    STR R3, [R0, #( 3*10 + 2 )*4]
+    STR R3, [R0, #( 3*10 + 3 )*4]
+    STR R3, [R0, #( 3*10 + 6 )*4]
+    STR R3, [R0, #( 3*10 + 7 )*4]
+    STR R3, [R0, #( 4*10 + 2 )*4]
+    STR R3, [R0, #( 4*10 + 6 )*4]
+    STR R3, [R0, #( 4*10 + 7 )*4]
+    STR R3, [R0, #( 4*10 + 8 )*4]
+    STR R3, [R0, #( 5*10 + 2 )*4]
+    STR R3, [R0, #( 5*10 + 6 )*4]
+    STR R3, [R0, #( 5*10 + 7 )*4]
+    STR R3, [R0, #( 5*10 + 8 )*4]
+    STR R3, [R0, #( 6*10 + 2 )*4]
+    STR R3, [R0, #( 6*10 + 6 )*4]
+    STR R3, [R0, #( 6*10 + 7 )*4]
 
-    STR R3, [R0, #(3*10 + 2)*4]
-    STR R3, [R0, #(3*10 + 6)*4]
-    STR R3, [R0, #(3*10 + 7)*4]
-    STR R3, [R0, #(3*10 + 8)*4]
+@ ===== Nivel1 ===== Manzanas
+    LDR R0, =0x2000
+    MOV R3, #5
 
-    STR R3, [R0, #(4*10 + 2)*4]
-    STR R3, [R0, #(4*10 + 6)*4]
-    STR R3, [R0, #(4*10 + 7)*4]
-    STR R3, [R0, #(4*10 + 8)*4]
-
-    STR R3, [R0, #(5*10 + 2)*4]
-    STR R3, [R0, #(5*10 + 6)*4]
-    STR R3, [R0, #(5*10 + 7)*4]
+    STR R3, [R0, #( 0*10 + 3 )*4]
+    STR R3, [R0, #( 0*10 + 5 )*4]
+    STR R3, [R0, #( 0*10 + 7 )*4]
 
     @ Cabeza 1 y cuerpo 2
     MOV R3, #1
-    STR R3, [R0, #(1*10 + 1)*4]   
+    STR R3, [R0, #(0*10 + 1)*4]   
     MOV R3, #2
-    STR R3, [R0, #(1*10 + 2)*4]   
+    STR R3, [R0, #(0*10 + 0)*4]   
     BX LR
 	
 @ NIVEL 2
@@ -99,31 +105,31 @@ fill_loop2:
     STR R2, [R0], #4
     SUBS R12, R12, #1
     BNE fill_loop2
+	
+@ ===== Nivel2 =====
     LDR R0, =0x2000
     MOV R3, #4
 
-     @ Valor 4: obstaculos
-    STR R3, [R0, #(0*10 + 0)*4]
-    STR R3, [R0, #(0*10 + 3)*4]
-    STR R3, [R0, #(0*10 + 6)*4]
-    STR R3, [R0, #(0*10 + 9)*4]
-    STR R3, [R0, #(3*10 + 0)*4]
-    STR R3, [R0, #(3*10 + 3)*4]
-    STR R3, [R0, #(3*10 + 6)*4]
-    STR R3, [R0, #(3*10 + 9)*4]
-    STR R3, [R0, #(6*10 + 0)*4]
-    STR R3, [R0, #(6*10 + 3)*4]
-    STR R3, [R0, #(6*10 + 6)*4]
-    STR R3, [R0, #(6*10 + 9)*4]
-    STR R3, [R0, #(9*10 + 0)*4]
-    STR R3, [R0, #(9*10 + 3)*4]
-    STR R3, [R0, #(9*10 + 6)*4]
-    STR R3, [R0, #(9*10 + 9)*4]
-
-    STR R3, [R0, #(2*10 + 2)*4]
-    STR R3, [R0, #(2*10 + 7)*4]
-    STR R3, [R0, #(7*10 + 2)*4]
-    STR R3, [R0, #(7*10 + 7)*4]
+    STR R3, [R0, #( 0*10 + 0 )*4]
+    STR R3, [R0, #( 0*10 + 3 )*4]
+    STR R3, [R0, #( 0*10 + 6 )*4]
+    STR R3, [R0, #( 0*10 + 9 )*4]
+    STR R3, [R0, #( 2*10 + 2 )*4]
+    STR R3, [R0, #( 2*10 + 7 )*4]
+    STR R3, [R0, #( 3*10 + 0 )*4]
+    STR R3, [R0, #( 3*10 + 3 )*4]
+    STR R3, [R0, #( 3*10 + 6 )*4]
+    STR R3, [R0, #( 3*10 + 9 )*4]
+    STR R3, [R0, #( 6*10 + 0 )*4]
+    STR R3, [R0, #( 6*10 + 3 )*4]
+    STR R3, [R0, #( 6*10 + 6 )*4]
+    STR R3, [R0, #( 6*10 + 9 )*4]
+    STR R3, [R0, #( 7*10 + 2 )*4]
+    STR R3, [R0, #( 7*10 + 7 )*4]
+    STR R3, [R0, #( 9*10 + 0 )*4]
+    STR R3, [R0, #( 9*10 + 3 )*4]
+    STR R3, [R0, #( 9*10 + 6 )*4]
+    STR R3, [R0, #( 9*10 + 9 )*4]
 
     @ Cabeza 1 y cuerpo 2
     MOV R3, #1
@@ -141,55 +147,47 @@ fill_loop3:
     STR R2, [R1], #4
     SUBS R12, R12, #1
     BNE fill_loop3
-    LDR R1, =0x2000
+
+@ ===== Nivel3 =====
+    LDR R0, =0x2000
     MOV R3, #4
 
-     @ Valor 4: obstaculos
-    STR R3, [R1, #(0*10 + 0)*4]
-    STR R3, [R1, #(0*10 + 2)*4]
-    STR R3, [R1, #(0*10 + 4)*4]
-    STR R3, [R1, #(0*10 + 6)*4]
-    STR R3, [R1, #(0*10 + 8)*4]
-    STR R3, [R1, #(1*10 + 0)*4]
-    STR R3, [R1, #(1*10 + 4)*4]
-    STR R3, [R1, #(1*10 + 8)*4]
-    STR R3, [R1, #(2*10 + 0)*4]
-    STR R3, [R1, #(2*10 + 2)*4]
-    STR R3, [R1, #(2*10 + 4)*4]
-    STR R3, [R1, #(2*10 + 6)*4]
-    STR R3, [R1, #(2*10 + 8)*4]
-    STR R3, [R1, #(3*10 + 0)*4]
-    STR R3, [R1, #(3*10 + 6)*4]
-    STR R3, [R1, #(3*10 + 9)*4]
-    STR R3, [R1, #(4*10 + 2)*4]
-    STR R3, [R1, #(4*10 + 4)*4]
-    STR R3, [R1, #(4*10 + 6)*4]
-    STR R3, [R1, #(5*10 + 3)*4]
-    STR R3, [R1, #(5*10 + 5)*4]
-    STR R3, [R1, #(6*10 + 0)*4]
-    STR R3, [R1, #(6*10 + 2)*4]
-    STR R3, [R1, #(6*10 + 4)*4]
-    STR R3, [R1, #(6*10 + 6)*4]
-    STR R3, [R1, #(6*10 + 8)*4]
-    STR R3, [R1, #(7*10 + 0)*4]
-    STR R3, [R1, #(7*10 + 4)*4]
-    STR R3, [R1, #(7*10 + 8)*4]
-    STR R3, [R1, #(8*10 + 0)*4]
-    STR R3, [R1, #(8*10 + 2)*4]
-    STR R3, [R1, #(8*10 + 4)*4]
-    STR R3, [R1, #(8*10 + 6)*4]
-    STR R3, [R1, #(8*10 + 8)*4]
-    STR R3, [R1, #(9*10 + 0)*4]
-    STR R3, [R1, #(9*10 + 2)*4]
-    STR R3, [R1, #(9*10 + 4)*4]
-    STR R3, [R1, #(9*10 + 6)*4]
-    STR R3, [R1, #(9*10 + 8)*4]
+    STR R3, [R0, #( 0*10 + 0 )*4]
+    STR R3, [R0, #( 0*10 + 1 )*4]
+    STR R3, [R0, #( 0*10 + 8 )*4]
+    STR R3, [R0, #( 0*10 + 9 )*4]
+    STR R3, [R0, #( 1*10 + 0 )*4]
+    STR R3, [R0, #( 1*10 + 6 )*4]
+    STR R3, [R0, #( 1*10 + 9 )*4]
+    STR R3, [R0, #( 2*10 + 3 )*4]
+    STR R3, [R0, #( 2*10 + 4 )*4]
+    STR R3, [R0, #( 2*10 + 5 )*4]
+    STR R3, [R0, #( 2*10 + 6 )*4]
+    STR R3, [R0, #( 3*10 + 3 )*4]
+    STR R3, [R0, #( 4*10 + 1 )*4]
+    STR R3, [R0, #( 4*10 + 3 )*4]
+    STR R3, [R0, #( 4*10 + 8 )*4]
+    STR R3, [R0, #( 5*10 + 1 )*4]
+    STR R3, [R0, #( 5*10 + 6 )*4]
+    STR R3, [R0, #( 5*10 + 8 )*4]
+    STR R3, [R0, #( 6*10 + 6 )*4]
+    STR R3, [R0, #( 7*10 + 3 )*4]
+    STR R3, [R0, #( 7*10 + 4 )*4]
+    STR R3, [R0, #( 7*10 + 5 )*4]
+    STR R3, [R0, #( 7*10 + 6 )*4]
+    STR R3, [R0, #( 8*10 + 0 )*4]
+    STR R3, [R0, #( 8*10 + 3 )*4]
+    STR R3, [R0, #( 8*10 + 9 )*4]
+    STR R3, [R0, #( 9*10 + 0 )*4]
+    STR R3, [R0, #( 9*10 + 1 )*4]
+    STR R3, [R0, #( 9*10 + 8 )*4]
+    STR R3, [R0, #( 9*10 + 9 )*4]
 
     @ Cabeza 1 y cuerpo 2
     MOV R3, #1
-    STR R3, [R1, #(1*10 + 1)*4]   
+    STR R3, [R1, #(4*10 + 5)*4]   
     MOV R3, #2
-    STR R3, [R1, #(1*10 + 2)*4]   
+    STR R3, [R1, #(4*10 + 4)*4]   
     BX LR
 
 @ --- Simulación de crecimiento de la serpiente ---
@@ -212,8 +210,8 @@ LEFT_ARROW: .word 0xE04B
 RIGHT_ARROW:.word 0xE04D
 
 KEYS:                           @ Arreglo de teclas simuladas
+    .word 0xE050                @ Abajo
     .word 0xE04D                @ Derecha
     .word 0xE048                @ Arriba
-    .word 0xE04B                @ Izquierda
-    .word 0xE050                @ Abajo
+	.word 0xE04D                @ Derecha
     .word 0x1234                @ Tecla inválida
