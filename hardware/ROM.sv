3f4a7526f4fca8a68145c71654c4c30ad6ddcf62
ejbm10 <eduardo.bm10@estudiantec.cr>
2025-06-05 23:56:17 -0600

    prueba con beq y bne

--- hardware/ROM.sv
+++ hardware/ROM.sv
@@ -5,19 +5,22 @@ module ROM (
 	output logic [31:0] inst
 );
 	
-	logic [31:0] instruction_set [0:3];
+	logic [31:0] instruction_set [0:6];
 	
 	initial begin
-		instruction_set[0] = 32'hE3A02005;
-		instruction_set[1] = 32'hE3A03008;
-		instruction_set[2] = 32'hE0834005;
-		instruction_set[3] = 32'hEA00002A;
+		instruction_set[0] = 32'hE3A02005;	// MOV R2, #5
+		instruction_set[1] = 32'hE3A03008;	// MOV R3, #8
+		instruction_set[2] = 32'hE0824003;	// ADD R4, R2, R3
+		instruction_set[3] = 32'hE354000D;	// CMP R4, #13
+		instruction_set[4] = 32'h1A00002A;	// BNE [OFFSET RARO]
+		instruction_set[5] = 32'hE354000D;	// CMP R4, #13
+		instruction_set[6] = 32'h0A00002A;	// BEQ [OFFSET RARO]
 	end
 	
 	always_ff @(posedge clk or posedge rst) begin
 		if (rst)
 			inst <= 32'h0;
-		else if (PC >> 2 < 4)
+		else if (PC >> 2 < 7)
 			inst <= instruction_set[PC >> 2];
 	end
 	
